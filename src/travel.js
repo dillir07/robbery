eb1fef71a053a8ca0752f9b8b5e0c229c51672a7
Dilli Babu R <dr@ciena.com>
2022-01-12 13:42:06 +0530

    botathon

---
+++ src/travel.js
@@ -0,0 +1,7 @@
+const idValueEl = document.querySelector("#idValue")
+const results = document.querySelector("#results")
+
+document.querySelector("#btnSearch").addEventListener("click", () => {
+    results.innerHTML = "<h1>Woh! Slowdown detective. This phase is not unlocked yet, check out timeline tab</h1>"
+    return
+})
